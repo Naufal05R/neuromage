0827d1718ac9dfe101ee8025d8aa44a3bdf223b6
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-07 15:49:54 +0700

    feat: add children props

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const AuthenticationLayout = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const AuthenticationLayout = ({ children }: { children: React.ReactNode }) => {
+  return <main className="auth">{children}</main>;
+};
 
-export default AuthenticationLayout
+export default AuthenticationLayout;
