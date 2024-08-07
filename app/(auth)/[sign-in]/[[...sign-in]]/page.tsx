2e6a20eaadea0f43c7625218fed363e0ee91efb5
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-07 17:54:53 +0700

    feat: add SignIn component from clerk

--- app/(auth)/[sign-in]/[[...sign-in]]/page.tsx
+++ app/(auth)/[sign-in]/[[...sign-in]]/page.tsx
@@ -1,11 +1,10 @@
+import { SignIn } from '@clerk/nextjs'
 import React from 'react'
 
-const page = () => {
+const SignInPage = () => {
   return (
-    <div>
-      
-    </div>
+    <SignIn />
   )
 }
 
-export default page
+export default SignInPage
