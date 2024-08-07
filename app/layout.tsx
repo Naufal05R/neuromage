22800bafc32934c07676e8343f1b5a11f5884ea6
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-07 09:31:13 +0700

    initializing fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const IBM_PLEX_SANS = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex-sans",
+});
 
 export const metadata: Metadata = {
   title: "Neuromage",
@@ -16,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn("font-IBM_PLEX_SANS antialiased", IBM_PLEX_SANS.variable)}>{children}</body>
     </html>
   );
 }
