aadad8272bf4a616c16fdb492057a2edce8a6367
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-08 10:03:45 +0700

    feat: add navlist

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,17 +1,64 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { navLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
-    <aside className='sidebar'>
-        <div className="flex size-full flex-col gap-4">
-            <Link href='/' className='sidebar-logo'>
-                <Image src="/assets/images/logo-text.svg" alt="logo" width={180} height={28} className="" />
-            </Link>
-        </div>
-    </aside>
-  )
-}
+    <aside className="sidebar">
+      <div className="flex size-full flex-col gap-4">
+        <Link href="/" className="sidebar-logo">
+          <Image
+            src="/assets/images/logo-text.svg"
+            alt="logo"
+            width={180}
+            height={28}
+            className=""
+          />
+        </Link>
 
-export default Sidebar
+        <nav className="sidebar-nav">
+          <SignedIn>
+            <ul className="sidebar-nav_elements">
+              {navLinks.map((link, index) => {
+                const isActive = link.route === pathname;
+
+                return (
+                  <li
+                    key={link.route}
+                    className={cn(
+                      "sidebar-nav_element group",
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700",
+                    )}
+                  >
+                    <Link className="sidebar-link" href={link.route}>
+                      <Image
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={cn({ "brightness-200": isActive })}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </SignedIn>
+        </nav>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
