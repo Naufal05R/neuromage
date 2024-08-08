b2f8566e70cf99caae46e00002e33c353f3d17af
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-08 09:00:39 +0700

    feat: pretec the page when user is signed-out

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,16 @@
+import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 import React from "react";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
       <div className="root-container">
-        <div className="wrapper">{children}</div>
+        <SignedIn>
+          <div className="wrapper">{children}</div>
+        </SignedIn>
+        <SignedOut>
+          <RedirectToSignIn />
+        </SignedOut>
       </div>
     </main>
   );
