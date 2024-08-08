2c8683f9ae0951717cb55e580eff6b3523776b85
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-08 10:04:49 +0700

    feat: add sign out button

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -55,6 +55,12 @@ const Sidebar = () => {
               })}
             </ul>
           </SignedIn>
+
+          <SignedOut>
+            <Button asChild className="button bg-purple-gradient bg-cover">
+              <Link href="/sign-in">Sign In</Link>
+            </Button>
+          </SignedOut>
         </nav>
       </div>
     </aside>
