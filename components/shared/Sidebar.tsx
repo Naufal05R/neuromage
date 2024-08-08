b8e5bfe483ccae2fc2fdeeb733ac45f7e7c362e5
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-08 11:13:26 +0700

    refactor: moving Navlink component  to /components/shared/base/ folder

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,42 +1,9 @@
-"use client";
-
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { navLinks } from "@/constants";
-import { cn } from "@/lib/utils";
 import { Button } from "../ui/button";
-
-const NavLink = ({ start, end }: { start: number; end?: number }) => {
-  const pathname = usePathname();
-
-  return navLinks.slice(start, end).map((link) => {
-    const isActive = link.route === pathname;
-
-    return (
-      <li
-        key={link.route}
-        className={cn(
-          "sidebar-nav_element group",
-          isActive ? "bg-purple-gradient text-white" : "text-gray-700",
-        )}
-      >
-        <Link className="sidebar-link" href={link.route}>
-          <Image
-            src={link.icon}
-            alt="logo"
-            width={24}
-            height={24}
-            className={cn({ "brightness-200": isActive })}
-          />
-          {link.label}
-        </Link>
-      </li>
-    );
-  });
-};
+import { NavLink } from "./base";
 
 const Sidebar = () => {
   return (
