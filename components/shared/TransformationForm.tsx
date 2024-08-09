cd9dca96b939c1cbbd43ccaca217089f5fd9fd1d
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-09 16:52:48 +0700

    fix: remove unnecessary import for some variables

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -13,15 +13,8 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
-import { Button } from "@/components/ui/button";
 import {
   Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { aspectRatioOptions, defaultValues, transformationTypes } from "@/constants";
