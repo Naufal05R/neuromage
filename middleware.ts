0f7d2e3159e4750bc8288ddf5eab6aa681f7a4ca
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-09 08:35:34 +0700

    fix: turn middleware.ts for using clerk v4 for preventing error

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,18 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// clerk v5
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+// const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect();
-  }
+// export default clerkMiddleware((auth, request) => {
+//   if (!isPublicRoute(request)) {
+//     auth().protect();
+//   }
+// });
+
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  publicRoutes: ["/api/webhooks/clerk"],
 });
 
 export const config = {
