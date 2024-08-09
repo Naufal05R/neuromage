4bdb2805a6244ad8a8e73ef1188c650b56708df0
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-09 09:21:36 +0700

    change middleware.ts to v4 clerk

--- middleware.ts
+++ middleware.ts
@@ -16,8 +16,5 @@ export default authMiddleware({
 });
 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
