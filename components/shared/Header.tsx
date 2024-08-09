7062bad1db9df5ed0f52115a02d001fea28e66ef
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-09 13:33:49 +0700

    feat: Add Title & SubTitle to Header.tsx component

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({ title, subTitle }: { title: string, subTitle?: string }) => {
   return (
-    <div>
-      
-    </div>
+    <>
+        <h2 className='h2-bold text-dark-600'>
+            {title}
+        </h2>
+        {subTitle && (
+          <p className='p-16-regular mt-4'>
+            {subTitle}
+          </p>
+        )}
+    </>
   )
 }
 
