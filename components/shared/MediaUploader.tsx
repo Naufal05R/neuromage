ff962e4ee09f4c3822801f1e83b5988fa1373964
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-10 10:12:27 +0700

    feat: creating CldUploadWidget & it function handler

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -1,13 +1,37 @@
 import React from 'react'
 import { useToast } from '../ui/use-toast'
+import { CldUploadWidget } from 'next-cloudinary'
 
 const MediaUploader = () => {
   const { toast } = useToast();
 
+  const onUploadSuccessHandler = (result: any) => {
+    toast({
+      title: "Image upload successfully",
+      description: "1 credit was deducted from your account",
+      duration: 5000,
+      className: "success-toast",
+    });
+  }
+  const onUploadErrorHandler = () => {
+    toast({
+      title: 'Something went wrong while uploading',
+      description: 'Please try again',
+      duration: 5000,
+      className: 'error-toast',
+    })
+  }
+
   return (
-    <div>
-      MediaUploader
-    </div>
+    <CldUploadWidget 
+        uploadPreset='neuromage'
+        options={{
+            multiple: false,
+            resourceType: 'image',
+        }}
+        onSuccess={onUploadSuccessHandler}
+        onError={onUploadErrorHandler}
+    />
   )
 }
 
