123719ebf963602159383d745eb024239a35dc75
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-12 13:43:01 +0700

    feat: create addImage function

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -1,6 +1,10 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+import Image from "../database/models/image.model";
 import User from "../database/models/user.model";
+import { connectToDatabase } from "../database/mongoose";
+import { handleError } from "../utils";
 
 const populateUser = (query: any) =>
   query.populate({
@@ -8,3 +12,25 @@ const populateUser = (query: any) =>
     model: User,
     select: "_id firstName lastName",
   });
+
+// ADD IMAGE
+export async function addImage({ image, userId, path }: AddImageParams) {
+  try {
+    await connectToDatabase();
+
+    const author = await User.findById(userId);
+
+    if (!author) throw new Error("User not found");
+
+    const newImage = await Image.create({
+      ...image,
+      author: author._id,
+    });
+
+    revalidatePath(path);
+
+    return JSON.parse(JSON.stringify(newImage));
+  } catch (error) {
+    handleError(error);
+  }
+}
