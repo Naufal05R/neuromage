63537cab3f0c83140f3937c3c1899c4e568e36bc
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-12 14:46:42 +0700

    feat: creating code for action Update

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -27,7 +27,7 @@ import MediaUploader from "./MediaUploader";
 import TransformedImage from "./TransformedImage";
 import { updateCredits } from "@/lib/actions/user.actions";
 import { getCldImageUrl } from "next-cloudinary";
-import { addImage } from "@/lib/actions/image.actions";
+import { addImage, updateImage } from "@/lib/actions/image.actions";
 import { useRouter } from "next/navigation";
 
 export const formSchema = z.object({
@@ -109,6 +109,25 @@ const TransformationForm = ({
             form.reset();
             setImage(data);
             router.push(`/transformations/${newImage._id}`);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      
+      if (action === "Update") {
+        try {
+          const updatedImage = await updateImage({
+            image: {
+              ...imageData,
+              _id: data?._id,
+            },
+            userId,
+            path: `/transformations/${data._id}`,
+          });
+
+          if (updatedImage) {
+            router.push(`/transformations/${updatedImage._id}`);
           }
         } catch (error) {
           console.log(error);
