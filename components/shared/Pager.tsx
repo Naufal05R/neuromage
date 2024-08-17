94bef245c27f9268ab81134d666644192cf94455
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-17 23:00:56 +0700

    feat: make this Pager component returning Pagination component

--- components/shared/Pager.tsx
+++ components/shared/Pager.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { formUrlQuery } from "@/lib/utils";
+import { Pagination, PaginationContent, PaginationNext, PaginationPrevious } from "../ui/pagination";
+import { Button } from "../ui/button";
 
 const Pager = () => {
   const router = useRouter();
@@ -17,6 +19,36 @@ const Pager = () => {
 
     router.push(newUrl, { scroll: false });
   };
+
+  return (
+    <>
+      {totalPages > 1 && (
+        <Pagination className="mt-10">
+          <PaginationContent className="flex w-full">
+            <Button
+              disabled={Number(page) <= 1}
+              className="collection-btn"
+              onClick={() => onPageChange("prev")}
+            >
+              <PaginationPrevious className="hover:bg-transparent hover:text-white" />
+            </Button>
+
+            <p className="flex-center p-16-medium w-fit flex-1">
+              {page} / {totalPages}
+            </p>
+
+            <Button
+              className="button w-32 bg-cover text-white bg-gradient-sky-blue"
+              onClick={() => onPageChange("next")}
+              disabled={Number(page) >= totalPages}
+            >
+              <PaginationNext className="hover:bg-transparent hover:text-white" />
+            </Button>
+          </PaginationContent>
+        </Pagination>
+      )}
+    </>
+  );
 };
 
 export default Pager;
