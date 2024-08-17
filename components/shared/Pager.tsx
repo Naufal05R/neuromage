5460ac62f93cd97ee2c9df923c75ada768fd7550
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-17 23:00:04 +0700

    feat: creating onPageChange change page handler function

--- components/shared/Pager.tsx
+++ components/shared/Pager.tsx
@@ -1,9 +1,22 @@
-import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery } from "@/lib/utils";
 
 const Pager = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
+
+  const onPageChange = (action: string) => {
+    const pageValue = action === "next" ? Number(page) + 1 : Number(page) - 1;
+
+    const newUrl = formUrlQuery({
+      searchParams: searchParams.toString(),
+      key: "page",
+      value: pageValue,
+    });
+
+    router.push(newUrl, { scroll: false });
+  };
 };
 
 export default Pager;
