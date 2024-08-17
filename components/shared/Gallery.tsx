a5f5cea632ff097e7d294e2a7930276b3c512320
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-17 23:32:32 +0700

    feat: fetching data in the Gallery.tsx component file

--- components/shared/Gallery.tsx
+++ components/shared/Gallery.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { Collection } from "./Collection";
+import { getAllImages } from "@/lib/actions/image.actions";
 
-const Gallery = async ({ page, searchQuery }: { page: number, searchQuery: string }) => {
+const Gallery = async ({
+  page,
+  searchQuery,
+}: {
+  page: number;
+  searchQuery: string;
+}) => {
+  const images = await getAllImages({ page, searchQuery });
+
   return (
     <section className="sm:mt-12">
       <Collection
