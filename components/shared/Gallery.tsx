ca43453793b6b9e6be0f8fded6fb42eb4fc35f39
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-18 00:30:11 +0700

    fix: make a good style in fallback suspense

--- components/shared/Gallery.tsx
+++ components/shared/Gallery.tsx
@@ -24,7 +24,13 @@ const Gallery = ({
         fallback={
           <ul className="collection-list">
             {Array.from({ length: 10 }).map((_, index) => (
-              <li key={index} className="collection-card" />
+              <li key={index} className="collection-card">
+                <div className="h-52 w-full rounded-[10px] bg-slate-400" />
+                <div className="flex-between">
+                  <div className="h-7 rounded-full bg-slate-400 odd:w-2/4 even:w-3/4"></div>
+                  <div className="flex-center h-6 w-6 rounded-full bg-slate-400" />
+                </div>
+              </li>
             ))}
           </ul>
         }
