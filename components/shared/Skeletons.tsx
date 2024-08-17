e102a041a9721ec07e719ee5d52731c50ec16ad4
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-18 01:29:18 +0700

    feat: updating skeleton using shimmer

--- components/shared/Skeletons.tsx
+++ components/shared/Skeletons.tsx
@@ -2,11 +2,14 @@ export const GalleryImageSkeleton = () => {
   return (
     <ul className="collection-list">
       {Array.from({ length: 10 }).map((_, index) => (
-        <li key={index} className="collection-card">
-          <div className="h-52 w-full rounded-[10px] bg-slate-400" />
+        <li
+          key={index}
+          className="collection-card before:animate-shimmer relative overflow-hidden before:absolute before:inset-0 before:-translate-x-full before:bg-gradient-to-r before:from-transparent before:from-20% before:via-slate-400/20 before:to-80% before:to-transparent"
+        >
+          <div className="h-52 w-full rounded-[10px] bg-slate-400/20" />
           <div className="flex-between">
-            <div className="h-7 rounded-full bg-slate-400 odd:w-2/4 even:w-3/4"></div>
-            <div className="flex-center h-6 w-6 rounded-full bg-slate-400" />
+            <div className="h-7 rounded-full bg-slate-400/20 odd:w-2/4 even:w-3/4"></div>
+            <div className="flex-center h-6 w-6 rounded-full bg-slate-400/20" />
           </div>
         </li>
       ))}
