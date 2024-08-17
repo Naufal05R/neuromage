ab6b1e0ad9f3d4e2d7618fe2ed3c881ac56b7985
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-17 23:59:26 +0700

    fix: remove unnecessary import

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Gallery from "@/components/shared/Gallery";
