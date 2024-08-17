6e0d7bcc3daba3b67b2907a5ce2b8fa84e5627a9
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-17 23:32:06 +0700

    fix: fixing page props type in the Gallery component

--- components/shared/Gallery.tsx
+++ components/shared/Gallery.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Collection } from "./Collection";
 
-const Gallery = async ({ page, searchQuery }: { page: string | number, searchQuery: string }) => {
+const Gallery = async ({ page, searchQuery }: { page: number, searchQuery: string }) => {
   return (
     <section className="sm:mt-12">
       <Collection
