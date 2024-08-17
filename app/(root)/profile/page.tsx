87ff71527e83e59aa25d7912e0e5bcf5135cdd04
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-18 01:37:59 +0700

    fix: change from directly using Collection Component by using Gallery component instead

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 import { redirect } from "next/navigation";
 
 import Header from "@/components/shared/Header";
-import { Collection } from "@/components/shared/Collection";
 import { getUserImages } from "@/lib/actions/image.actions";
 import { getUserById } from "@/lib/actions/user.actions";
+import Gallery from "@/components/shared/Gallery";
 
 const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
@@ -50,13 +50,10 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
         </div>
       </section>
 
-      <section className="mt-8 md:mt-14">
-        <Collection
-          images={images?.data}
-          totalPages={images?.totalPages}
-          page={page}
-        />
-      </section>
+      <Gallery
+        page={page}
+        heading="Your Collections"
+      />
     </>
   );
 };
