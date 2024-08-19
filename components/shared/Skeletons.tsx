2472fca5c03cf954d7905bfd09d3bea40bee5541
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-19 11:21:59 +0700

    fix: changing styles.shimmer to skeleton from styles

--- components/shared/Skeletons.tsx
+++ components/shared/Skeletons.tsx
@@ -1,10 +1,6 @@
 import { MAX_IMAGES_PER_PAGE } from "@/constants";
+import { skeleton } from "@/constants/styles";
 import { cn } from "@/lib/utils";
-
-const styles = {
-  shimmer:
-    "relative overflow-hidden before:absolute before:inset-0 before:-translate-x-full before:animate-shimmer before:bg-gradient-to-r before:from-transparent before:from-20% before:via-slate-400/20 before:to-transparent before:to-80%",
-};
 
 export const GalleryImageSkeleton = () => {
   return (
@@ -14,20 +10,20 @@ export const GalleryImageSkeleton = () => {
           <div
             className={cn(
               "h-52 w-full rounded-[10px] bg-slate-400/20",
-              styles.shimmer,
+              skeleton,
             )}
           />
           <div className="flex-between">
             <div
               className={cn(
                 "h-7 rounded-full bg-slate-400/20 odd:w-2/4 even:w-3/4",
-                styles.shimmer,
+                skeleton,
               )}
             />
             <div
               className={cn(
                 "flex-center h-6 w-6 rounded-full bg-slate-400/20",
-                styles.shimmer,
+                skeleton,
               )}
             />
           </div>
@@ -45,20 +41,20 @@ export const ProfileInformationSkeleton = () => {
           <div
             className={cn(
               "h-[22px] w-full rounded-full bg-slate-400/20",
-              styles.shimmer,
+              skeleton,
             )}
           />
           <div className="mt-4 flex items-center gap-4">
             <div
               className={cn(
                 "size-9 rounded-full bg-slate-400/20 md:size-12",
-                styles.shimmer,
+                skeleton,
               )}
             />
             <div
               className={cn(
                 "h-[39px] w-2/3 rounded-full bg-slate-400/20",
-                styles.shimmer,
+                skeleton,
               )}
             />
           </div>
