c1ac64cee0d8b82e0df1074cb8bbbdcc425349f9
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-19 10:21:24 +0700

    feat: make a good loading skeleton

--- app/(root)/profile/loading.tsx
+++ app/(root)/profile/loading.tsx
@@ -1,4 +1,35 @@
+import Image from "next/image";
+
 export default function Loading() {
-  // Or a custom loading skeleton component
-  return <p>Loading...</p>;
+  return (
+    <section className="profile">
+      <div className="profile-balance">
+        <p className="p-14-medium md:p-16-medium">CREDITS AVAILABLE</p>
+        <div className="mt-4 flex items-center gap-4">
+          <Image
+            src="/assets/icons/coins.svg"
+            alt="coins"
+            width={50}
+            height={50}
+            className="size-9 md:size-12"
+          />
+          <div className="h-[39px] rounded-full w-16 bg-slate-400" />
+        </div>
+      </div>
+
+      <div className="profile-image-manipulation">
+        <p className="p-14-medium md:p-16-medium">IMAGE MANIPULATION DONE</p>
+        <div className="mt-4 flex items-center gap-4">
+          <Image
+            src="/assets/icons/photo.svg"
+            alt="coins"
+            width={50}
+            height={50}
+            className="size-9 md:size-12"
+          />
+          <div className="h-[39px] rounded-full w-12 bg-slate-400" />
+        </div>
+      </div>
+    </section>
+  );
 }
