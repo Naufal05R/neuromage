a1e2bed675cc78a882448f60e9ea90d9eeea5b69
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-19 11:12:27 +0700

    feat: changing from long className into a simple styles.shimmer in GalleryImageSkeleton function

--- components/shared/Skeletons.tsx
+++ components/shared/Skeletons.tsx
@@ -10,17 +10,26 @@ export const GalleryImageSkeleton = () => {
   return (
     <ul className="collection-list">
       {Array.from({ length: MAX_IMAGES_PER_PAGE }).map((_, index) => (
-        <li
-          key={index}
-          className={cn(
-            "collection-card",
-            "relative overflow-hidden before:absolute before:inset-0 before:-translate-x-full before:animate-shimmer before:bg-gradient-to-r before:from-transparent before:from-20% before:via-slate-400/20 before:to-transparent before:to-80%",
-          )}
-        >
-          <div className="h-52 w-full rounded-[10px] bg-slate-400/20" />
+        <li key={index} className="collection-card">
+          <div
+            className={cn(
+              "h-52 w-full rounded-[10px] bg-slate-400/20",
+              styles.shimmer,
+            )}
+          />
           <div className="flex-between">
-            <div className="h-7 rounded-full bg-slate-400/20 odd:w-2/4 even:w-3/4"></div>
-            <div className="flex-center h-6 w-6 rounded-full bg-slate-400/20" />
+            <div
+              className={cn(
+                "h-7 rounded-full bg-slate-400/20 odd:w-2/4 even:w-3/4",
+                styles.shimmer,
+              )}
+            />
+            <div
+              className={cn(
+                "flex-center h-6 w-6 rounded-full bg-slate-400/20",
+                styles.shimmer,
+              )}
+            />
           </div>
         </li>
       ))}
@@ -32,13 +41,7 @@ export const ProfileInformationSkeleton = () => {
   return (
     <ul className="profile">
       {Array.from({ length: 2 }).map((_, index) => (
-        <li
-          key={index}
-          className={cn(
-            "profile-card_information",
-            "relative overflow-hidden before:absolute before:inset-0 before:-translate-x-full before:animate-shimmer before:bg-gradient-to-r before:from-transparent before:from-20% before:via-slate-400/20 before:to-transparent before:to-80%",
-          )}
-        >
+        <li key={index} className="profile-card_information">
           <div className="h-[22px] w-full rounded-full bg-slate-400/20" />
           <div className="mt-4 flex items-center gap-4">
             <div className="size-9 rounded-full bg-slate-400/20 md:size-12" />
