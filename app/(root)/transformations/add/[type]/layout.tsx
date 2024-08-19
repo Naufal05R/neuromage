e61fb962e67f55895f55c96609f55fc805e03b06
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-19 13:41:34 +0700

    feat: changing from using loading.tsx file to manually using React Suspense

--- app/(root)/transformations/add/[type]/layout.tsx
+++ app/(root)/transformations/add/[type]/layout.tsx
@@ -1,5 +1,7 @@
 import Header from "@/components/shared/Header";
+import AddTransformationTypeSkeleton from "@/components/shared/Skeletons";
 import { transformationTypes } from "@/constants";
+import { Suspense } from "react";
 
 export default function AddTransformationTypeLayout({
   children,
@@ -12,7 +14,15 @@ export default function AddTransformationTypeLayout({
   return (
     <>
       <Header title={transformation.title} subtitle={transformation.subTitle} />
-      {children}
+      <Suspense
+        fallback={
+          <AddTransformationTypeSkeleton
+            type={transformation.type as TransformationTypeKey}
+          />
+        }
+      >
+        {children}
+      </Suspense>
     </>
   );
 }
