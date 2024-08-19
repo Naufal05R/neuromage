4eed26f6d31a877d266946902ba50f0c7810acc5
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-19 11:20:51 +0700

    feat: create a new styles.ts constant

---
+++ constants/styles.ts
@@ -0,0 +1,6 @@
+const styles = {
+  skeleton:
+    "relative overflow-hidden before:absolute before:inset-0 before:-translate-x-full before:animate-shimmer before:bg-gradient-to-r before:from-transparent before:from-20% before:via-slate-400/20 before:to-transparent before:to-80%",
+};
+
+export const { skeleton } = styles;
