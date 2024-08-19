5a7c9d4ba5c8ad19b2213809e52f7194939bfccd
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-19 10:25:36 +0700

    feat: adding ProfileInformation component to the ProfilePage

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -17,35 +17,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
 
   return (
     <>
-      <section className="profile">
-        <div className="profile-balance">
-          <p className="p-14-medium md:p-16-medium">CREDITS AVAILABLE</p>
-          <div className="mt-4 flex items-center gap-4">
-            <Image
-              src="/assets/icons/coins.svg"
-              alt="coins"
-              width={50}
-              height={50}
-              className="size-9 md:size-12"
-            />
-            <h2 className="h2-bold text-dark-600">{user.creditBalance}</h2>
-          </div>
-        </div>
-
-        <div className="profile-image-manipulation">
-          <p className="p-14-medium md:p-16-medium">IMAGE MANIPULATION DONE</p>
-          <div className="mt-4 flex items-center gap-4">
-            <Image
-              src="/assets/icons/photo.svg"
-              alt="coins"
-              width={50}
-              height={50}
-              className="size-9 md:size-12"
-            />
-            <h2 className="h2-bold text-dark-600">{images?.data.length}</h2>
-          </div>
-        </div>
-      </section>
+      <ProfileInformation userId={userId} page={page} />
 
       <Gallery page={page} heading="Your Collections" />
     </>
