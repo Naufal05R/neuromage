788e9e29f4fc1e06aea6ea4927d313cefcc7cc7d
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-08-19 11:10:45 +0700

    feat: creating new styles object variable

--- components/shared/Skeletons.tsx
+++ components/shared/Skeletons.tsx
@@ -1,6 +1,11 @@
 import { MAX_IMAGES_PER_PAGE } from "@/constants";
 import { cn } from "@/lib/utils";
 
+const styles = {
+  shimmer:
+    "relative overflow-hidden before:absolute before:inset-0 before:-translate-x-full before:animate-shimmer before:bg-gradient-to-r before:from-transparent before:from-20% before:via-slate-400/20 before:to-transparent before:to-80%",
+};
+
 export const GalleryImageSkeleton = () => {
   return (
     <ul className="collection-list">
