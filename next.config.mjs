bd309500abcd7e2e63b7ac2066f249e619839c99
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2025-03-12 16:21:14 +0700

    chore(next): update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,35 +9,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "frame-ancestors *",
-          },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: '*',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'ALLOWALL',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
